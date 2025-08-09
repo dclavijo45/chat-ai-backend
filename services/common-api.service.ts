e2b1ed3e1b7b2d94b1e2872f5aa32d144f6249b5
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2025-08-09 13:56:03 -0500

    Update: change OpenAI model version to gpt-5-2025-08-07

--- services/common-api.service.ts
+++ services/common-api.service.ts
@@ -82,7 +82,7 @@ export class CommonApiService {
         );
 
         const models = {
-            [AiEngineEnum.OPENAI]: 'gpt-4.1-2025-04-14',
+            [AiEngineEnum.OPENAI]: 'gpt-5-2025-08-07',
             [AiEngineEnum.DEEPSEEK]: DeepSeeKModelsEnum.DEEPSEEK_CHAT,
             [AiEngineEnum.QWENAI]: 'qwen-max-latest',
             [AiEngineEnum.QWENAI_VISION]: 'qwen-vl-max',
